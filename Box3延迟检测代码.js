b4d5c09430630435b8da13c6369fee21de42c49a
全能岛民 <69710718+qndm@users.noreply.github.com>
2021-01-15 16:05:21 +0800

    Box3延迟检测代码.js
    检测Box3的延迟

---
+++ Box3延迟检测代码.js
@@ -0,0 +1,13 @@
+let number_of_tests = 0;
+let qualified_number = 0;
+world.onTick(async({tick,prevTick,elapsedTimeMS,skip})=>{
+    number_of_tests ++;
+    if(!skip){
+        qualified_number ++;
+    }
+    if(skip){
+        console.error(`时刻${tick}\n上一时刻${prevTick}\n间隔${elapsedTimeMS/1000}秒\n延迟${skip}\n延迟数${number_of_tests-qualified_number}\n--------------------`)
+    }else{
+        console.log(`时刻${tick}\n上一时刻${prevTick}\n间隔${elapsedTimeMS/1000}秒\n延迟${skip}\n延迟数${number_of_tests-qualified_number}\n--------------------`)
+    }
+})
