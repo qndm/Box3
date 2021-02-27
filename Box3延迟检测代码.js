80c963ed0118f7495842ef08706fd2e5f946d8bc
全能岛民 <69710718+qndm@users.noreply.github.com>
2021-02-27 11:39:12 +0800

    Update Box3延迟检测代码.js

--- Box3延迟检测代码.js
+++ Box3延迟检测代码.js
@@ -6,8 +6,8 @@ world.onTick(async({tick,prevTick,elapsedTimeMS,skip})=>{
         qualified_number ++;
     }
     if(skip){
-        console.error(`时刻${tick}\n上一时刻${prevTick}\n间隔${elapsedTimeMS/1000}秒\n延迟${skip}\n延迟数${number_of_tests-qualified_number}\n--------------------`)
+        console.error(`时刻${tick}\n上一时刻${prevTick}\n间隔${elapsedTimeMS/1000}秒\n延迟${skip}\n延迟数${number_of_tests-qualified_number}\n--------------------`);
     }else{
-        console.log(`时刻${tick}\n上一时刻${prevTick}\n间隔${elapsedTimeMS/1000}秒\n延迟${skip}\n延迟数${number_of_tests-qualified_number}\n--------------------`)
+        console.log(`时刻${tick}\n上一时刻${prevTick}\n间隔${elapsedTimeMS/1000}秒\n延迟${skip}\n延迟数${number_of_tests-qualified_number}\n--------------------`);
     }
-})
+});
